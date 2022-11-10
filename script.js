5e1db9f02f4fba96b27ccda0c10ff6038c428c86
Gavin O'Brien <113049201+Gavobro@users.noreply.github.com>
2022-11-10 13:22:44 -0400

    Update script.js
    Assigned variables to lowercase, uppercase, and number values.
    Created a prompt and asked how long your personal password will be from 8-128 characters in length.
    Created checkboxes asking if the user wants lowercase, uppercase and/or numbers in their password.
    Made a randomizer for the values of the password.
    Cleaned up code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// My code here.
 // Assigning variables to lowercase, uppercase, and number values.
 function generatePassword () {
   var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
